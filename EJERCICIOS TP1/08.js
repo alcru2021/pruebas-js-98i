38bb0f9825c80817741ca9ea27a7c420e6300a9a
Alfredo <prueba@correo.com>
2024-03-25 13:32:15 -0300

    enviando 08.js

--- EJERCICIOS TP1/08.js
+++ EJERCICIOS TP1/08.js
@@ -10,20 +10,13 @@ Output: El 15 no es divisible por 2.
 */
 
 
-let numero = parseInt(prompt("Ingrese un número: 10"));
+let numero = Number(prompt("Ingrese un número:"));
 
-// Verificar si el número es divisible por 2
+// Verifico si el número es divisible por 2
 if (numero % 2 === 0) {
     document.write("El " + numero + " es divisible por 2.<br>");
 } else {
     document.write("El " + numero + " no es divisible por 2.<br>");
 }
 
-let numero2 = parseInt(prompt("Ingrese un número: 15"));
 
-// Verificar si el número es divisible por 2
-if (numero2 % 2 === 0) {
-    document.write("El " + numero2 + " es divisible por 2.<br>");
-} else {
-    document.write("El " + numero2 + " no es divisible por 2.<br>");
-}
