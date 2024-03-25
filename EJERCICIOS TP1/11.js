39e6acff0ed79ae1a1e8dd0357ee1fc318bc9ba1
Alfredo <prueba@correo.com>
2024-03-25 13:33:18 -0300

    enviando 11.js

--- EJERCICIOS TP1/11.js
+++ EJERCICIOS TP1/11.js
@@ -13,44 +13,21 @@ Output: El 210 es divisible por 2, por 3, por 5 y por 7.
 
 */
 
-let numero = parseInt(prompt("Ingrese un número: 20"));
 
-// Verifico si el número es divisible por 2
+
+const numero = Number(prompt("Por favor, ingresa un número:"));
+
+
 if (numero % 2 === 0) {
-    document.write("El " + numero + " es divisible por 2.<br>")
+    document.write(numero + " es divisible por 2." );
 }
-
-// Verifico si el número es divisible por 5
+if (numero % 3 === 0) {
+    document.write(numero + " es divisible por 3.");
+}
 if (numero % 5 === 0) {
-    document.write("El " + numero + " es divisible por 5.<br>");
+    document.write(numero + " es divisible por 5.");
+}
+if (numero % 7 === 0) {
+    document.write(numero + " es divisible por 7.");
 }
 
-//----------------------------------------------------------------
-
-let numero2 = parseInt(prompt("Ingrese un número: 210"));
-
-// Verifico si el número es divisible por 2
-if (numero2 % 2 === 0) {
-    document.write("El " + numero2 + " es divisible por 2.<br>")
-}
-
-// Verifico si el número es divisible por 3
-if (numero2 % 3 === 0) {
-    document.write("El " + numero2 + " es divisible por 3.<br>");
-} else {
-    document.write("El " + numero2 + " no es divisible por 3.<br>");
-}
-
-// Verifico si el número es divisible por 5
-if (numero2 % 5 === 0) {
-    document.write("El " + numero2 + " es divisible por 5.<br>");
-} else {
-    document.write("El " + numero2 + " no es divisible por 5.<br>");
-}
-
-// Verifico si el número es divisible por 7
-if (numero2 % 7 === 0) {
-    document.write("El " + numero2 + " es divisible por 7.<br>");
-} else {
-    document.write("El " + numero2 + " no es divisible por 7.<br>");
-}
